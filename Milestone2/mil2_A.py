86b660d0fe166c30666cfca6f88260f6a4b90477
Mithilesh V <62344690+Mithilesh-V@users.noreply.github.com>
2022-03-08 16:27:01 +0530

    milA_2 changes

--- Milestone2/mil2_A.py
+++ Milestone2/mil2_A.py
@@ -3,6 +3,8 @@ import threading
 import yaml
 import time
 import logging
+import pandas as pd
+compare_val = dict()
 
 LOG_FILENAME = 'logging_example12.txt'
 Format = "%(asctime)s.%(msecs)06d;%(message)s"
@@ -13,25 +15,100 @@ logging.basicConfig(
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 
-with open("Milestone1B.yaml","r") as stream:
+with open("Milestone2A.yaml","r") as stream:
     parsed_yaml = yaml.safe_load(stream)
+
+def timefunc_cond(dicti,initial_str):
+    traverse = dicti['Condition'].split(" ")
+    logging.info(initial_str+" "+"Entry")
+    if traverse[0][2:len(traverse[0])-1] in compare_val.keys():
+        if traverse[1] == '>':
+            print("yessss")
+            if compare_val[traverse[0][2:len(traverse[0])-1]] >int(traverse[2]):
+                logging.info(initial_str+" "+"Executing"+" "+dicti['Function']+" "+"("+str(dicti['Inputs']['FunctionInput'])+', '+str(dicti['Inputs']['ExecutionTime'])+")")
+                k = dicti['Inputs']['ExecutionTime']
+                time.sleep(int(k))
+                logging.info(initial_str+" "+"Exit")
+        elif traverse[1] =='<':
+            if compare_val[traverse[0][2:len(traverse[0])-1]] <int(traverse[2]):
+                logging.info(initial_str+" "+"Executing"+" "+dicti['Function']+" "+"("+str(dicti['Inputs']['FunctionInput'])+', '+str(dicti['Inputs']['ExecutionTime'])+")")
+                k = dicti['Inputs']['ExecutionTime']
+                time.sleep(int(k))
+                logging.info(initial_str+" "+"Exit")
+
+        
+
 
 def timefunc(input_val):
     k = input_val['ExecutionTime']
     time.sleep(int(k))
+def loader_cond(input,condi,outputs,string,initial_str,dicti):
+    traverse = condi.split(" ")
+    logging.info(initial_str+" "+"Entry")
+    if traverse[0][2:len(traverse[0])-1] in compare_val.keys():
+        if traverse[1] == '>':
+            print("yessss")
+            if compare_val[traverse[0][2:len(traverse[0])-1]] >int(traverse[2]):
+                logging.info(initial_str+" "+"Executing"+" "+dicti['Function']+" "+"("+str(dicti['Inputs']['Filename'])+")")
+                data_table = pd.read_csv(input['Filename'])
+                k = data_table.shape
+                defects = k[0]
+                print(defects)
+                new_string = string+'NoOfDefects'
+                compare_val[new_string] = defects
+                print(compare_val)
+                logging.info(initial_str+" "+"Exit")
+        
+        elif traverse[1] =='<':
+            print("Yes")
+            logging.info(initial_str+" "+"Executing"+" "+dicti['Function']+" "+"("+str(dicti['Inputs']['Filename'])+")")
+            if compare_val[traverse[0][2:len(traverse[0])-1]] <int(traverse[2]):
+                data_table = pd.read_csv(input['Filename'])
+                k = data_table.shape
+                defects = k[0]
+                print(defects)
+                new_string = string+'NoOfDefects'
+                compare_val[new_string] = defects
+                print(compare_val)
+                logging.info(initial_str+" "+"Exit")
 
-def task_perform(func,input_val,initial_str):
-    if func == 'TimeFunction':
-        logging.info(initial_str+" "+"Entry")
-        logging.info(initial_str+" "+"Executing"+" "+func+" "+"("+str(input_val['FunctionInput'])+', '+str(input_val['ExecutionTime'])+")")
-        timefunc(input_val)
-        logging.info(initial_str+" "+"Exit")
-    print(func,input_val,initial_str)
+
+def loader(input,outputs,strings):
+    data_table = pd.read_csv(input['Filename'])
+    k = data_table.shape
+    defects = k[0]
+    print(defects)
+    new_string = strings+'NoOfDefects'
+    compare_val[new_string] = defects
+    print(compare_val)
+
+def task_perform(dict_obj,initial_str):
+    if dict_obj['Function'] == 'TimeFunction':
+        if 'Condition' in dict_obj.keys():
+            timefunc_cond(dict_obj,initial_str)
+        else:
+            logging.info(initial_str+" "+"Entry")
+            logging.info(initial_str+" "+"Executing"+" "+dict_obj['Function']+" "+"("+str(dict_obj['Inputs']['FunctionInput'])+', '+str(dict_obj['Inputs']['ExecutionTime'])+")")
+            timefunc(dict_obj['Inputs'])
+            logging.info(initial_str+" "+"Exit")
+
+    if dict_obj['Function'] =="DataLoad":
+        #logging.info(initial_str+" "+"Executing"+" "+dict_obj['Function']+" "+"("+str(dict_obj['Inputs']['Filename'])+")")
+        print(dict_obj.keys())
+        if 'Condition' in dict_obj.keys():
+            loader_cond(dict_obj['Inputs'],dict_obj['Condition'],dict_obj['Outputs'],initial_str+'.',initial_str,dict_obj)
+        else:
+            logging.info(initial_str+" "+"Entry")
+            logging.info(initial_str+" "+"Executing"+" "+dict_obj['Function']+" "+"("+str(dict_obj['Inputs']['Filename'])+")")
+            loader(dict_obj['Inputs'],dict_obj['Outputs'],initial_str+'.')
+            logging.info(initial_str+" "+"Exit")
+
+    #print(func,dict_obj['Inputs'],initial_str)
 
 
 def func(dict_obj,initial_string):
         if dict_obj['Type'] =='Task':
-            task_perform(dict_obj['Function'],dict_obj['Inputs'],initial_string)
+            task_perform(dict_obj,initial_string)
         elif dict_obj['Type'] =='Flow' and dict_obj['Execution'] =='Sequential':
             logging.info(initial_string+" "+"Entry")
             for key in dict_obj['Activities'].keys():
@@ -44,8 +121,8 @@ def func(dict_obj,initial_string):
             arr = []
             for i in m:
                 arr.append(dict_obj['Activities'][i])
-            print(arr)
-            print(s)
+            #print(arr)
+            #print(s)
             join_arr=[]
             logging.info(initial_string+" "+"Entry")
             for i in range(len(arr)):
